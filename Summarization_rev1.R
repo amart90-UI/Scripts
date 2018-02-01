ff2dd07e93039b1b714a4bd61e9b5ee41e4ddceb
amart90 <anthonymartinez90@gmail.com>
2018-01-31 18:48:11 -0800

    Incorporate Arjans revisions (1)
    Topo:
    - Remove redundant variables (rough, cos.asp, SWASP)
    - remove 3x3 version of TPI and TRI
    - go back to faceted graphs
    - remove titles
    
    Summ:
    -work on updating barplot
    -4 bars
    - incorporate cover type

---
+++ Summarization_rev1.R
@@ -0,0 +1,220 @@
+# Setup
+setwd("U:/Refugia/Persistance/")
+library(raster)
+library(rgdal)
+library(reshape2)
+library(ggplot2)
+library(plyr)
+
+# Load data
+fire <- readOGR("Data/perims_dissolve.shp")
+fire.data <- fire@data
+#ui <- readOGR("Data/ui_overlap.shp")
+#ui.data <- ui@data[,-1]
+#write.csv(ui.data, "Intermediates/OverlapData.csv")
+#ui.data <- read.csv("Intermediates/OverlapData.csv")
+ui.data <- read.csv("Intermediates/OverlapLndCvr.csv")
+#ui.data <- ui.data[, c(2:5]
+ui.data$Cvr <- rep(1:2, length.out = 816802)
+ui.data$overlap <- factor(ui.data$overlap)
+
+Forest <- ui.data[ui.data$Cvr == 1,]
+Range <- ui.data[ui.data$Cvr == 2,]
+
+# Fire boundary statistics
+fire.area <- sum(fire.data$Area)
+
+# Shape indices
+ui.data$FRAC <- 2 * log(.25 * ui.data$Perim) / log(ui.data$AREA) # Fractal dimension index
+ui.data$PARA <- ui.data$Perim / ui.data$Area # Perimeter-Area Ratio
+# Summary stats: http://www.umass.edu/landeco/research/fragstats/documents/Metrics/Shape%20Metrics/SHAPE%20METRICS.htm
+
+# Area summary
+area.stats <- ddply(ui.data, "overlap", summarise,
+                    total = sum(AREA), 
+                    prop.persist = sum(AREA), 
+                    min = min(AREA), 
+                    max = max(AREA),
+                    mean = mean(AREA), 
+                    area.median = median(AREA), 
+                    sd = sd(AREA), 
+                    se = sd(AREA)/sqrt(length(AREA)))
+area.stats$prop.persist <- c(0, area.stats$prop.persist[-1] / sum(area.stats$total[-1]))
+
+# Proportions
+prop.stats <- ddply(ui.data, "overlap", summarise,
+                    count = length(overlap), 
+                    count.persist = length(overlap), 
+                    count.unburned = length(overlap)/length(ui.data$AREA), 
+                    area.fire = sum(AREA) / fire.area,
+                    area.unburned = sum(AREA) / sum(ui.data$AREA),
+                    area.persist = sum(AREA),
+                    area.persist.2 = sum(AREA))
+prop.stats$count.persist <- c(0, prop.stats$count.persist[-1] / sum(prop.stats$count.persist[-1]))
+prop.stats$area.persist <- c(0, prop.stats$area.persist[-1] / sum(area.stats$total[-1]))
+prop.stats$area.persist.2 <- c(0, 0, prop.stats$area.persist.2[-c(1:2)] / sum(area.stats$total[-c(1:2)]))
+
+# Shape index summary
+shape.stats <- ddply(ui.data, "overlap", summarise,
+                     FRAC.mean = mean(FRAC), 
+                     FRAC.median = median(FRAC), 
+                     FRAC.sd = sd(FRAC), 
+                     FRAC.AreaWtMean = sum(FRAC * AREA), 
+                     PARA.mean = mean(PARA), 
+                     PARA.median = median(PARA), 
+                     FRAC.sd = sd(PARA), 
+                     PARA.AreaWtMean = sum(PARA * AREA))
+shape.stats$FRAC.AreaWtMean <- shape.stats$FRAC.AreaWtMean / area.stats$total
+shape.stats$PARA.AreaWtMean <- shape.stats$PARA.AreaWtMean / area.stats$total
+
+# Construct dataframe
+stats <- data.frame(cbind(prop.stats, area.stats[,-1], shape.stats[,-1]))
+stats$overlap <- factor(stats$overlap)
+#write.csv(stats, "Persitsent_stats.csv")
+#read.csv("Persitsent_stats.csv")
+
+# Proportion by area plot
+lvl <- c("Burned", "Unburned for 1 fire", "Unburned for 2 fires", "Unburned for 3 fires", "Unburned for 4 fires")
+area.prop <- data.frame(overlap = c(lvl, lvl[2:5],lvl[3:5]),
+                        values = c(1-sum(prop.stats$area.fire), prop.stats[,5], prop.stats[,6], prop.stats[-1,7]),
+                        group = factor(c(rep("Within fire perimeters", times = 5), rep("Among unburned islands", times = 4), 
+                                         rep("Among persistant unburned islands", times = 3))))
+area.prop$group <- factor(area.prop$group, levels = rev(levels(area.prop$group)))
+
+ggplot(aes(y = values, x = group, fill = overlap), data = area.prop) +
+  geom_bar(position = position_fill(reverse = T), stat="identity") +
+  labs(title = "", x = "", y = "Proportion of area") +
+  guides(fill=guide_legend(title="Degree of persistence"))
+
+# Stacked shape plots
+med <- ddply(ui.data, "overlap", summarise, # calculate medians
+             AREA.med = median(AREA), PARA.med = median(PARA), FRAC.med = median(FRAC))
+med$overlap <- factor(med$overlap)
+
+ggplot(ui.data, aes(x=AREA, colour = factor(overlap))) + 
+  geom_density(position = "identity", fill = NA, size = 1) +
+  geom_vline(data = med, aes(xintercept = AREA.med, colour=overlap), linetype = "dashed") +
+  scale_x_continuous(breaks = seq(0, 15000, 2500), limits=c(0, 15000)) +
+  labs(title = expression(paste("Unburned island area (", m^2, ") distribution by degree of persistance")), 
+       x = expression(paste("Area (", m^2, ")")), y = "Density", colour = "Degree of\npersistence")
+ggplot(ui.data, aes(x=PARA, colour = factor(overlap))) + 
+  geom_density(position = "identity", fill = NA, size = 1) +
+  geom_vline(data = med, aes(xintercept = PARA.med, colour=overlap), linetype = "dashed") +
+  labs(title = "Unburned island perimeter area ratio (PARA) distribution by degree of persistance", x = "PARA", y = "Density", colour = "Degree of\npersistence")
+ggplot(ui.data, aes(x=FRAC, colour = factor(overlap))) + 
+  geom_density(position = "identity", fill = NA, size = 1) +
+  geom_vline(data = med, aes(xintercept = FRAC.med, colour=overlap), linetype = "dashed") +
+  scale_x_continuous(limits=c(1, 1.25)) +
+  labs(title = "Unburned island Fractional Dimension Index (FRAC) distribution by degree of persistance", x = "FRAC", y = "Density", colour = "Degree of\npersistence")
+
+# Partition data for testing
+ui.1 <- ui.data[ui.data$overlap == 1,]
+ui.2 <- ui.data[ui.data$overlap == 2,]
+ui.3 <- ui.data[ui.data$overlap == 3,]
+ui.4 <- ui.data[ui.data$overlap == 4,]
+
+# K-S test
+Area.ks <- round(c(ks.test(ui.1$AREA, ui.2$AREA)$p.value,
+                   ks.test(ui.1$AREA, ui.3$AREA)$p.value,
+                   ks.test(ui.1$AREA, ui.4$AREA)$p.value,
+                   ks.test(ui.2$AREA, ui.3$AREA)$p.value,
+                   ks.test(ui.2$AREA, ui.4$AREA)$p.value,
+                   ks.test(ui.3$AREA, ui.4$AREA)$p.value), digits = 4)
+PARA.ks <- round(c(ks.test(ui.1$PARA, ui.2$PARA)$p.value,
+                   ks.test(ui.1$PARA, ui.3$PARA)$p.value,
+                   ks.test(ui.1$PARA, ui.4$PARA)$p.value,
+                   ks.test(ui.2$PARA, ui.3$PARA)$p.value,
+                   ks.test(ui.2$PARA, ui.4$PARA)$p.value,
+                   ks.test(ui.3$PARA, ui.4$PARA)$p.value), digits = 4)
+FRAC.ks <- round(c(ks.test(ui.1$FRAC, ui.2$FRAC)$p.value,
+                   ks.test(ui.1$FRAC, ui.3$FRAC)$p.value,
+                   ks.test(ui.1$FRAC, ui.4$FRAC)$p.value,
+                   ks.test(ui.2$FRAC, ui.3$FRAC)$p.value,
+                   ks.test(ui.2$FRAC, ui.4$FRAC)$p.value,
+                   ks.test(ui.3$FRAC, ui.4$FRAC)$p.value), digits = 4)
+shape.ks <- data.frame(Area = Area.ks, PARA = PARA.ks, FRAC = FRAC.ks)
+
+# K-W test
+shape.kw <- round(c(kruskal.test(AREA ~ overlap, data = ui.data)$p.value,
+                    kruskal.test(PARA ~ overlap, data = ui.data)$p.value,
+                    kruskal.test(FRAC ~ overlap, data = ui.data)$p.value), digits = 4)
+
+# Combine statistical testing table
+shape.tests <- rbind(shape.ks, shape.kw)
+row.names(shape.tests) <- c("1 vs. 2", "1 vs. 3", "1 vs. 4", "2 vs. 3", "2 vs. 4", "3 vs. 4", "Kruskal-Wallis")
+
+
+########## strat by cover
+area.stats.f <- ddply(Forest, "overlap", summarise,
+                    total = sum(AREA), 
+                    prop.persist = sum(AREA), 
+                    min = min(AREA), 
+                    max = max(AREA),
+                    mean = mean(AREA), 
+                    area.median = median(AREA), 
+                    sd = sd(AREA), 
+                    se = sd(AREA)/sqrt(length(AREA)))
+area.stats.f$prop.persist <- c(0, area.stats.f$prop.persist[-1] / sum(area.stats.f$total[-1]))
+area.stats.f$Cvr <- rep(1, times = 4)
+
+area.stats.r <- ddply(Range, "overlap", summarise,
+                      total = sum(AREA), 
+                      prop.persist = sum(AREA), 
+                      min = min(AREA), 
+                      max = max(AREA),
+                      mean = mean(AREA), 
+                      area.median = median(AREA), 
+                      sd = sd(AREA), 
+                      se = sd(AREA)/sqrt(length(AREA)))
+area.stats.r$prop.persist <- c(0, area.stats.r$prop.persist[-1] / sum(area.stats.r$total[-1]))
+area.stats.r$Cvr <- rep(2, times = 4)
+
+# Proportions
+prop.stats.f <- ddply(Forest, "overlap", summarise,
+                    area.fire = sum(AREA) / fire.area,
+                    area.unburned = sum(AREA) / sum(Forest$AREA),
+                    area.persist = sum(AREA))
+prop.stats.f$area.persist <- c(0, prop.stats.f$area.persist[-1] / sum(area.stats.f$total[-1]))
+
+prop.stats.r <- ddply(Range, "overlap", summarise,
+                      area.fire = sum(AREA) / fire.area,
+                      area.unburned = sum(AREA) / sum(Range$AREA),
+                      area.persist = sum(AREA))
+prop.stats.r$area.persist <- c(0, prop.stats.f$area.persist[-1] / sum(area.stats.f$total[-1]))
+
+
+
+
+area.prop <- data.frame(overlap = c("Burned", 1,2,3,4,1,2,3,4,2,3,4),
+                        values = c(1-sum(prop.stats$area.fire), prop.stats[,5], prop.stats[,6], prop.stats[-1,7]),
+                        group = factor(c(rep("Within fire perimeters", times = 5), rep("Among unburned islands", times = 4), 
+                                         rep("Among persistant unburned islands", times = 3))))
+area.prop$overlap <- factor(as.character(c("Burned", 1,2,3,4,1,2,3,4,2,3,4)), levels = c("Burned", 1,2,3,4))
+area.prop$group <- factor(area.prop$group, levels = rev(levels(area.prop$group)))
+area.prop$values <- 100 *area.prop$values
+
+ggplot(aes(y = values, x = group, fill = overlap), data = area.prop) +
+  geom_bar(position = position_fill(reverse = T), stat="identity") +
+  labs(title = "Distribution of persistent unburned islands (area) by degree of persistence", x = "", y = "Proportion of area") +
+  guides(fill=guide_legend(title="Degree of\npersistence"))
+
+
+#####
+col <- c("#D55E00", "#f39c12", "#27ae60", "#2980b9", "#8e44ad")
+lvl <- c("Burned", "Unburned for 1 fire", "Unburned for 2 fires", "Unburned for 3 fires", "Unburned for 4 fires")
+area.prop <- data.frame(overlap = c(lvl, lvl[2:5],lvl[3:5], lvl[4:5]),
+                        values = c(1-sum(prop.stats$area.fire), prop.stats[,5], prop.stats[,6], prop.stats[-1,7], prop.stats[3:4,8]),
+                        group = factor(c(rep("Within fire perimeters", times = 5), rep("Among unburned islands", times = 4), 
+                                         rep("Among persistant unburned islands", times = 3), "a", "a")))
+area.prop$group <- factor(area.prop$group, levels = rev(levels(area.prop$group)))
+
+ggplot(aes(y = values, x = group, fill = overlap), data = area.prop) +
+  geom_bar(position = position_fill(reverse = T), stat="identity") +
+  labs(title = "", x = "", y = "Proportion of area") +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) +
+  scale_fill_manual("Degree of\npersistence", values = col)
+
+area.stats$total / 10000
+
+col <- c("#D55E00", "#f39c12", "#27ae60", "#2980b9", "#8e44ad")
+col <- c("#b33939", "#cd6133", "#cc8e35", "#ffda79", "#f7f1e3")
---
+++ TopoSummary_rev1.R
@@ -0,0 +1,143 @@
+# Setup
+setwd("U:/Refugia/Persistance/")
+library(ggplot2)
+library(plyr)
+
+# Load data
+sampl.data <- read.csv("valuetable.csv")
+sampl.data <- na.exclude(sampl.data)
+sampl.data$Unburned <- factor(sampl.data$Unburned)
+
+
+# Prep data (recode)
+LookUp <- data.frame(old = c(2,3,4,5,7,8,9,10,11,12), 
+                       new = c(1,2,2,2,2,3,2,2,3,3))
+#"Forest & Woodland", "Shrubland & Grassland", "Semi-Desert", 
+         #"Polar & High Montane Vegetation", "Nonvascular & Sparse Vascular Rock Vegetation", 
+         #"Agricultural Vegetation", "Introduced & Semi Natural Vegetation", 
+         #"Recently Disturbed or Modified", "Open Water", "Developed & Other Human Use"
+for(i in 1:nrow(LookUp)){
+  sampl.data$Cvr[sampl.data$LndCvr %in% LookUp$old[i]] <- LookUp$new[i]}
+sampl.data$Cvr[sampl.data$Cvr == 1] <- "Forest"
+sampl.data$Cvr[sampl.data$Cvr == 2] <- "Range"
+sampl.data$Cvr[sampl.data$Cvr == 3] <- "Other"
+Forest <- sampl.data[sampl.data[, "Cvr"] == "Forest",]
+Range <- sampl.data[sampl.data[, "Cvr"] == "Range",]
+sampl.data <- rbind(Forest, Range)
+sampl.data$Cvr <- as.factor(sampl.data$Cvr)
+
+# Group by unburned and cover type
+
+# Median values by group
+med <- ddply(sampl.data, c("Unburned", "Cvr"), summarise, # calculate medians
+             TPI.med = median(TPI), TPI90.med = median(TPI_90), TRI.med = median(TRI), TRI90.med = median(TRI_90),
+             rough.med = median(rough), rough90.med = median(rough_90), slope.med = median(slope), cosasp.med = median(cos.asp),
+             TRASP.med = median(TRASP), SWASP.med = median(SWASP), SCOSA.med = median(SCOSA))
+
+# Faceted kernel density estimation plots
+col <- c("#D55E00", "cyan3")
+
+ggplot(sampl.data, aes(x=TPI_90, colour=Unburned)) + 
+  geom_density(size = 1) +
+  geom_vline(data = med, aes(xintercept = TPI90.med, colour=Unburned), linetype = "dashed") +
+  facet_wrap(~Cvr)+
+  labs(title = "", x = "TPI", y = "Density") +
+  scale_color_manual("Dashed line \nas median", labels = c("Burned", "Persistent \nUnburned"), values = col)
+ggplot(sampl.data, aes(x=TRI_90, colour=Unburned)) + 
+  geom_density(size = 1) +
+  geom_vline(data = med, aes(xintercept = TRI90.med, colour=Unburned), linetype = "dashed") +
+  facet_wrap(~Cvr)+
+  labs(title = "", x = "TRI", y = "Density") +
+  scale_color_manual("Dashed line \nas median", labels = c("Burned", "Persistent \nUnburned"), values = col)
+ggplot(sampl.data, aes(x=slope, colour=Unburned)) + 
+  geom_density(size = 1) +
+  geom_vline(data = med, aes(xintercept = slope.med, colour=Unburned), linetype = "dashed") +
+  facet_wrap(~Cvr)+
+  labs(title = "", x = "Slope (°)", y = "Density") +
+  scale_color_manual("Dashed line \nas median", labels = c("Burned", "Persistent \nUnburned"), values = col)
+ggplot(sampl.data, aes(x=TRASP, colour=Unburned)) + 
+  geom_density(size = 1) +
+  geom_vline(data = med, aes(xintercept = TRASP.med, colour=Unburned), linetype = "dashed") +
+  facet_wrap(~Cvr)+
+  labs(title = "", x = "TRASP", y = "Density") +
+  scale_color_manual("Dashed line \nas median", labels = c("Burned", "Persistent \nUnburned"), values = col)
+ggplot(sampl.data, aes(x=SCOSA, colour=Unburned)) + 
+  geom_density(size = 1) +
+  geom_vline(data = med, aes(xintercept = SCOSA.med, colour=Unburned), linetype = "dashed") +
+  facet_wrap(~Cvr)+
+  labs(title = "", x = "SCOSA", y = "Density") +
+  scale_color_manual("Dashed line \nas median", labels = c("Burned", "Persistent \nUnburned"), values = col)
+
+# Build data frames
+TopoSummary <- data.frame(Variable = c("TPI", "TPI_90", "TRI", "TRI_90", "rough", "rough_90", "slope", "cos.asp", "TRASP", "SWASP", "SCOSA"))
+
+# K-S Tests
+pub <- sampl.data[sampl.data[, "Unburned"] == 1,]
+burn <- sampl.data[sampl.data[, "Unburned"] == 0,]
+TopoSummary$`K-S.Burn.vs.PUB` <- c(
+  ks.test(pub$TPI_90, burn$TPI_90)$p.value,
+  ks.test(pub$TRI_90, burn$TRI_90)$p.value,
+  ks.test(pub$slope, burn$slope)$p.value,
+  ks.test(pub$TRASP, burn$TRASP)$p.value,
+  ks.test(pub$SWASP, burn$SWASP)$p.value,
+  ks.test(pub$SCOSA, burn$SCOSA)$p.value)
+
+pub.f <- Forest[Forest[, "Unburned"] == 1,]
+burn.f <- Forest[Forest[, "Unburned"] == 0,]
+
+TopoSummary$`K-S.Burn.vs.PUB.Forest` <- c(
+  ks.test(pub.f$TPI_90, burn.f$TPI_90)$p.value,
+  ks.test(pub.f$TRI_90, burn.f$TRI_90)$p.value,
+  ks.test(pub.f$slope, burn.f$slope)$p.value,
+  ks.test(pub.f$TRASP, burn.f$TRASP)$p.value,
+  ks.test(pub.f$SCOSA, burn.f$SCOSA)$p.value)
+
+pub.r <- Range[Range[, "Unburned"] == 1,]
+burn.r <- Range[Range[, "Unburned"] == 0,]
+
+TopoSummary$`K-S.Burn.vs.PUB.Range` <- c(
+  ks.test(pub.r$TPI_90, burn.r$TPI_90)$p.value,
+  ks.test(pub.r$TRI_90, burn.r$TRI_90)$p.value,
+  ks.test(pub.r$slope, burn.r$slope)$p.value,
+  ks.test(pub.r$TRASP, burn.r$TRASP)$p.value,
+  ks.test(pub.r$SCOSA, burn.r$SCOSA)$p.value)
+
+TopoSummary$`K-S.Forest.vs.Range` <- c(
+  ks.test(Forest$TPI_90, Range$TPI_90)$p.value,
+  ks.test(Forest$TRI_90, Range$TRI_90)$p.value,
+  ks.test(Forest$slope, Range$slope)$p.value,
+  ks.test(Forest$TRASP, Range$TRASP)$p.value,
+  ks.test(Forest$SCOSA, Range$SCOSA)$p.value)
+
+# K-W Tests
+TopoSummary$`K-W.Burn.vs.PUB` <- c(
+  kruskal.test(TPI_90 ~ Unburned, data = sampl.data)$p.value,
+  kruskal.test(TRI_90 ~ Unburned, data = sampl.data)$p.value,
+  kruskal.test(slope ~ Unburned, data = sampl.data)$p.value,
+  kruskal.test(TRASP ~ Unburned, data = sampl.data)$p.value,
+  kruskal.test(SCOSA ~ Unburned, data = sampl.data)$p.value)
+
+TopoSummary$`K-W.Burn.vs.PUB.Forest` <- c(
+  kruskal.test(TPI_90 ~ Unburned, data = Forest)$p.value,
+  kruskal.test(TRI_90 ~ Unburned, data = Forest)$p.value,
+  kruskal.test(slope ~ Unburned, data = Forest)$p.value,
+  kruskal.test(TRASP ~ Unburned, data = Forest)$p.value,
+  kruskal.test(SCOSA ~ Unburned, data = Forest)$p.value)
+
+TopoSummary$`K-W.Burn.vs.PUB.Range` <- c(
+  kruskal.test(TPI_90 ~ Unburned, data = Range)$p.value,
+  kruskal.test(TRI_90 ~ Unburned, data = Range)$p.value,
+  kruskal.test(slope ~ Unburned, data = Range)$p.value,
+  kruskal.test(TRASP ~ Unburned, data = Range)$p.value,
+  kruskal.test(SCOSA ~ Unburned, data = Range)$p.value)
+
+TopoSummary$`K-W.Forest.vs.Range` <- c(
+  kruskal.test(TPI_90 ~ Cvr, data = sampl.data)$p.value,
+  kruskal.test(TRI_90 ~ Cvr, data = sampl.data)$p.value,
+  kruskal.test(slope ~ Cvr, data = sampl.data)$p.value,
+  kruskal.test(TRASP ~ Cvr, data = sampl.data)$p.value,
+  kruskal.test(SCOSA ~ Cvr, data = sampl.data)$p.value)
+
+TopoSummary[,-1] <- round(TopoSummary[,-1], digits = 4)
+
+# col <- c("#D55E00", "#0072B2", "#E69F00", "#56B4E9")
